af561eb615d3d83fd301dd3b4f1b5561b223b407
Rajesh Khadka <rajeshkhadkaofficial45@gmail.com>
2022-02-06 13:12:24 +0545

    React router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+} from "react-router-dom";
 
 const App = () => {
+  return (<>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/'>
 
-  return <div>
-    Hello
-  </div>;
-
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </>);
 };
 
 export default App;
+
 
