c2935642ec30e9ba240404757abc69c3cb27c595
Rajesh Khadka <rajeshkhadkaofficial45@gmail.com>
2022-02-08 11:32:24 +0545

    staging

--- server/server.js
+++ server/server.js
@@ -1,17 +1,15 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const dbCon = require("./config/db")
+const express = require("express");
+const path = require("path")
+const app = express();
+require("dotenv").config();
+const dbCon = require("./config/db");
 app.use(express.json({ extented: false }));
 
-app.get('/', (req, res) => {
-    res.send("Hello")
-})
-//create server and listen to the server 
+app.get("/", (req, res) => {
+
+});
+//create server and listen to the server
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-    console.log(`Backend running on ${port}`)
-})
-
- 
-
+  console.log(`Backend running on ${port}`);
+});
