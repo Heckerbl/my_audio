efedbd79e9c8ad07ee1983228fd02906cf5ec1c1
heckerbl <sarojregmi.officail@gmail.com>
2022-02-12 23:44:34 +0545

    database connection done and finally the data gets send to the database after clicking the search button

--- server/Routes/link.js
+++ server/Routes/link.js
@@ -2,7 +2,25 @@ const linkRouter = require("express").Router();
 const dbCon = require("../config/db");
 const YoutubeMp3Downloader = require("youtube-mp3-downloader");
 const pathToFfmpeg = require("ffmpeg-static");
-let { const { videoTitle, videoId, thumbnail, author } = data;, videoId, thumbnail, author } = data;
+
+const send_data = (data) => {
+  console.log(data);
+  let video_id = data.videoId;
+  let video_title = data.videoTitle;
+  let yt_channel = data.artist;
+  let thumbnail = data.thumbnail;
+  const sql =
+    "INSERT INTO audios (video_id,video_title,yt_channel , thumbnail, likes ,downloads ) VALUES(?,?,?,? ,?,?)";
+  dbCon.query(
+    sql,
+    [video_id, video_title, yt_channel, thumbnail, 0, 0],
+    (err, result) => {
+      if (err) throw err;
+      console.log("Record inderted");
+    }
+  );
+};
+
 const download_audio = (videoId) => {
   const YD = new YoutubeMp3Downloader({
     ffmpegPath: pathToFfmpeg,
@@ -14,21 +32,17 @@ const download_audio = (videoId) => {
   });
   YD.download(videoId, videoId + ".mp3");
   YD.on("finished", function (err, data) {
-    // console.log(JSON.stringify(data));
-    
-
+    send_data(data);
   });
 
   YD.on("error", function (error) {
-    console.log("there is an error");
+    console.log(error);
   });
 
   YD.on("progress", function (progress) {
     // console.log("The information after success ", JSON.stringify(progress));
   });
-
 };
-
 
 linkRouter.post("/getlinks", (req, res) => {
   let videoId = req.body.link.slice("32");
@@ -38,12 +52,6 @@ linkRouter.post("/getlinks", (req, res) => {
     (err, result) => {
       if (result.length === 0) {
         download_audio(videoId, videoId);
-        const sql =
-          "INSERT INTO audios (video_id,video_title,yt_channel) VALUES(?,?,?)";
-        dbCon.query(sql, [id, title, owner], (err, result) => {
-          if (err) throw err;
-          console.log("Record inderted");
-        })
       } else {
         console.log("data in the database already exist !!");
       }
