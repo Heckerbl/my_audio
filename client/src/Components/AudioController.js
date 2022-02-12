370832cbd7aa1cdb7c949f3b28dfb162db85a4ff
heckerbl <sarojregmi.official@gmail.com>
2022-02-12 13:54:44 +0545

    almost done the homepage ui part done

--- client/src/Components/AudioController.js
+++ client/src/Components/AudioController.js
@@ -1,33 +1,49 @@
-import React , {useState} from "react";
-import "../Styles/AudioContainer.css";
+import React, { useState } from "react";
+import "../Styles/AudioController.css";
 import img from "../images/temp/Ghosts.jpg";
+import { data } from "../context.js";
 
-const AudioContainer = () =>{
-const [Play , setPlay] =  useState(false);
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+const AudioContainer = () => {
+  const [Play, setPlay] = useState(false);
 
-
-    return  <div className="Audio">
-                <div className="song">  
-                    <div className="img">
-                        <img src = {img} alt="no image found" />
-                    </div>
-                    <div className="details">
- <div className="title"> Perfect | By ed sheeran and saroj collab  😘</div>
-                    <div className="author"> Saroj Regmi </div>
-                    </div>
-                   
-                </div>
-                <div className="customizable">
-                <div className="play_head"> <input type="range"/> </div>
-            <div className="controlls">
-                <div className="backward"></div>
-                <div className={Play ? "pause" : "play"} onClick={()=>{setPlay(!Play)}}></div>
-                <div className="forward"></div>
-            </div>
-                </div>
-                
+  return (
+    <div className="Audio">
+      <div className="Audio_song">
+        <div className="Audio_img">
+          <img src={img} alt="not found" />
         </div>
-     
-}
+        <div className="Audio_details">
+          <div className="Audio_title">{data.title} </div>
+          <div className="Audio_author"> {data.author} </div>
+        </div>
+      </div>
+      <div className="Audio-customizable">
+        <div className="Audio-play_head">
+          <input type="range" />
+        </div>
+        <div className="Audio-controlls">
+          <div className="Audio-backward">
+            <SkipPreviousIcon />
+          </div>
+          <div
+            className={Play ? "pause" : "play"}
+            onClick={() => {
+              setPlay(!Play);
+            }}
+          >
+            {Play ? <PauseIcon /> : <PlayArrowIcon />}
+          </div>
+          <div className="Audio-forward">
+            <SkipNextIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AudioContainer;
